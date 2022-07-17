af8ca17c654c847002a3469c741da506da3ff3ee
kashiflab <kashif.ali@salesassist.pk>
2022-07-17 19:45:31 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/tech/gogrocer_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tech.gogrocer_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/tech/groceryg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tech.groceryg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
